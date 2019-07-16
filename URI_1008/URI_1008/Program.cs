62b1c2b41206bbbfc4c7021c3a7f58bf5544393c
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:34:21 -0300

    1008 - Salário

---
+++ URI_1008/URI_1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1008
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int id, horas;
+			double valor;
+
+			id = int.Parse(Console.ReadLine());
+			horas = int.Parse(Console.ReadLine());
+			valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			Console.WriteLine("NUMBER = " + id);
+			Console.WriteLine("SALARY = U$ " + (valor * horas).ToString("F2", CultureInfo.InvariantCulture));
+		}
+	}
+}
