6a4392530e43fed1ff750081ccefc35185c4a1e1
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:47:33 -0300

    1013 - O Maior

---
+++ URI_1013/URI_1013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_1013
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, c, maiorAB, maiorABC;
+
+			string[] s = Console.ReadLine().Split(' ');
+
+			a = int.Parse(s[0]);
+			b = int.Parse(s[1]);
+			c = int.Parse(s[2]);
+
+			maiorAB = ((a + b) + Math.Abs(a - b)) / 2;
+			maiorABC = ((maiorAB + c) + Math.Abs(maiorAB - c)) / 2;
+
+			Console.WriteLine(maiorABC + " eh o maior"); // O resultado pedia essa linguagem informal.
+		}
+	}
+}
