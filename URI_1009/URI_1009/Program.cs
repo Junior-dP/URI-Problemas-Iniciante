1c0305c18d42280943e743efe58fdeec00473ba8
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:35:57 -0300

    1009 - Salário com Bônus

---
+++ URI_1009/URI_1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI_1009
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string nome;
+			double fixo, total;
+
+			nome = Console.ReadLine();
+			fixo = double.Parse(Console.ReadLine());
+			total = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("TOTAL = R$ " + (total / 100 * 15 + fixo).ToString("F2"));
+		}
+	}
+}
