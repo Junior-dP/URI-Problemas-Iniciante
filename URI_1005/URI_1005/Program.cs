5a1d853129fede051bce322578e483b3e43ecc94
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:30:11 -0300

    1005 - Média 1

---
+++ URI_1005/URI_1005/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1005
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a, b, media;
+
+			a = double.Parse(Console.ReadLine());
+			b = double.Parse(Console.ReadLine());
+			media = (a * 3.5 + b * 7.5) / 11;
+			Console.WriteLine("MEDIA = " + media.ToString("F5"));
+		}
+	}
+}
