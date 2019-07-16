6d21c7e1b592cd4e087b7978925cae8316994def
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:26:48 -0300

    1003 - Soma Simples

---
+++ URI_1003/URI_1003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1003
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, soma;
+
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			soma = a + b;
+			Console.WriteLine("SOMA = " + soma);
+		}
+	}
+}
