a446e062804feccb313ed9d2734c3cb7157570ca
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:16:30 -0300

    1001 - Extremamente Básico

---
+++ URI_1001/URI_1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1001
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, x;
+
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			x = a + b;
+			Console.WriteLine("X = " + x);
+		}
+	}
+}
