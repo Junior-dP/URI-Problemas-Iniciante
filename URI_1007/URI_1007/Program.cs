337dad4d32a0b28394b74933dc1518c0c47878dc
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:32:45 -0300

    1007 - Diferença

---
+++ URI_1007/URI_1007/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI_1007
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, c, d, diferenca;
+
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			c = int.Parse(Console.ReadLine());
+			d = int.Parse(Console.ReadLine());
+			diferenca = (a * b - c * d);
+			Console.WriteLine("DIFERENCA = " + diferenca);
+		}
+	}
+}
