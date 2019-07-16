18ee43a87861bccec8f4aaee72552efedfa7facb
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:31:41 -0300

    1006 - Média 2

---
+++ URI_1006/URI_1006/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI_1006
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a, b, c, media;
+
+			a = double.Parse(Console.ReadLine());
+			b = double.Parse(Console.ReadLine());
+			c = double.Parse(Console.ReadLine());
+			media = (a * 2.0 + b * 3.0 + c * 5.0) / (2.0 + 3.0 + 5.0);
+			Console.WriteLine("MEDIA = " + media.ToString("F1"));
+		}
+	}
+}
