bc66c5dc1e9a7ab101e0b1eba0947cf11639527e
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:18:36 -0300

    1002 - Área do Círculo

---
+++ URI_1002/URI_1002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1002
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double raio, n, area;
+
+			n = 3.14159;
+			raio = double.Parse(Console.ReadLine());
+			area = n * Math.Pow(raio, 2.0);
+			Console.WriteLine("A=" + area.ToString("F4"));
+		}
+	}
+}
---
+++ URI_1002/URI_1002/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("URI_1002")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("URI_1002")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("febd93f5-48c1-4f77-a134-bda33a6fc9fc")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
