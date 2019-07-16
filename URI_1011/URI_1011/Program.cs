e4f6a69e547f54e35a6dd6c2f58264b8dd2f600a
Junior de Paula <jhum.dp@gmail.com>
2019-07-16 17:44:22 -0300

    1011 - Esfera

---
+++ URI_1011/URI_1011/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI_1011
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double pi = 3.14159;
+			double raio, volume;
+
+			raio = double.Parse(Console.ReadLine());
+
+			volume = (4.0 / 3) * pi * Math.Pow(raio, 3);
+
+			Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+		}
+	}
+}
